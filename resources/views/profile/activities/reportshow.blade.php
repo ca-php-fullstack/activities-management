c65cae0a8086b5a470f3cdd6cdfb425269775720
rjovanoski <rade_j@hotmail.com>
2020-12-21 01:14:47 +0100

    Report Form Added

--- resources/views/profile/activities/reportshow.blade.php
+++ resources/views/profile/activities/reportshow.blade.php
@@ -7,13 +7,14 @@
         <div class="col-md-12 mt-3">
             <div class="card border-0 shadow mb-5 bg-white rounded">
                 <div class="card-header bg-dark text-white text-uppercase d-flex flex-wrap justify-content-between">
-                    <h2 class="text-warning mb-0">{{ __('Activities') }}</h2>
+                    <h2 class="text-warning mb-0">{{ __('Report') }}</h2>
                     <div class="d-flex justify-content-center col-md-6 p-0">
                         <input type="email" name="" id="" class="form-control mr-4 col-md-10" placeholder="E-Mail recipient for activity report">
                         <a href="" class="btn btn-secondary">{{ __('Send') }}</a>
                     </div>
                 </div>
-                    <div class="card-body">                   
+                <div class="card-body"> 
+                    <form action="" method="get">                 
                         <div class="row">
                             <div class="col-sm-6 mb-3">
                                 <div class="card">
@@ -32,8 +33,9 @@
                                 </div>
                             </div>
                         </div>                        
-                            <button type="submit" class="btn btn-dark px-4">Find Report</button>      
-                    </div>
+                        <button type="submit" class="btn btn-dark px-4">Find Report</button>     
+                    </form> 
+                </div>
             </div>
         </div>
     </div>
